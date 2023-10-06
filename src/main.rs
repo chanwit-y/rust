93adb08918ada7e81a7edaa750a546f1a49cc36e
cy <chanwit.yimneam@hotmail.com>
2023-10-06 14:22:56 +0700

    🍀

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-fn main() {
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn hello(data: web::Data<AppState>) -> impl Responder {
+
+    HttpResponse::Ok().body(format!(
+        "Hello {}!",
+        data.app_name
+    ))
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix-web"),
+            }))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
