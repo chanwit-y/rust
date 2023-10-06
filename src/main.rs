4a9f70bff0680c75348ded597ccbab5604dc6d0a
cy <chanwit.yimneam@hotmail.com>
2023-10-06 15:39:17 +0700

    🌼

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+
+use actix_web::{cookie::time::Weekday, get, post, web, App, HttpResponse, HttpServer, Responder};
 
 struct AppState {
     app_name: String,
+    counter: Mutex<i32>,
 }
 
 #[get("/")]
 async fn hello(data: web::Data<AppState>) -> impl Responder {
-
-    HttpResponse::Ok().body(format!(
-        "Hello {}!",
-        data.app_name
-    ))
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    HttpResponse::Ok().body(format!("Hello {} -> {}!", data.app_name, counter))
 }
 
 #[post("/echo")]
@@ -22,15 +23,20 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(AppState {
                 app_name: String::from("Actix-web"),
+                counter: Mutex::new(0),
             }))
             .service(hello)
+            .service(web::scope("/users")
+            .route(
+                "/get",
+                web::to(|| async { HttpResponse::Ok().body("users") }),
+            ))
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
