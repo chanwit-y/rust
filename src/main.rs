3d5196edcf6aab4125aa252c2a5526168e96fa92
cy <chanwit.yimneam@hotmail.com>
2023-10-07 15:59:12 +0700

    ❄️

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use actix_web::{web, App, HttpServer, HttpResponse};
 
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
+    .bind(("127.0.0.1", 8080))?.run().await
 }
